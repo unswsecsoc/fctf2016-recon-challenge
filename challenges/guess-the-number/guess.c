4f549f34a2d4ec1d35db5deeba90b99e91c34ef4
Andrew Bennett <andrew.bennett@unsw.edu.au>
2016-04-07 16:04:58 +1000

    okay let's try this I'll add the flag later

---
+++ challenges/guess-the-number/guess.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  printf("enter a number!\n");
+  int number;
+  scanf("%d", &number);
+  if (number == "0x41") {
+    printf("Nice work! Here's the flag: ");
+    printf("<insert flag here>");
+    printf("\n");
+  }
+  return 0;
+}
